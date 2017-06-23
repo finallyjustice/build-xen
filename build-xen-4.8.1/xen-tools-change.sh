a4fa028937ce9fd973695c6734463b378193407b
Dongli Zhang <dongli.zhang0129@gmail.com>
2017-06-23 14:58:02 +0800

    tar xvf

--- build-xen-4.8.1/xen-tools-change.sh
+++ build-xen-4.8.1/xen-tools-change.sh
@@ -2,7 +2,7 @@
 
 XENPATH=/home/zhang/src/xen-4.8.1
 
-tar seabios-rel-1.10.0.tar.gz
+tar xvf seabios-rel-1.10.0.tar.gz
 mv seabios-dir ${XENPATH}/tools/firmware/.
 
 cp ipxe-git-827dd1bfee67daa683935ce65316f7e0f057fe1c.tar.gz ${XENPATH}/tools/firmware/etherboot/ipxe.tar.gz
